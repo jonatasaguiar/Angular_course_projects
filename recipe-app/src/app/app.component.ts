6030d0b35adc9a55baa601054957601cbfbadfef
jonatas <jonatasaguiar2@gmail.com>
2021-12-11 08:16:42 -0500

    Added directives-related files.

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipe-app';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    console.log(`LoadedFeature Before ${this.loadedFeature}`);
+    this.loadedFeature = feature;
+    console.log(`LoadedFeature after ${this.loadedFeature}`);
+  }
 }
--- recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Output,
+  ViewChild,
+  EventEmitter,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  amountInput: any;
 
-  constructor() { }
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+  onAddItem(amountInput) {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = Number(this.amountInputRef.nativeElement.value);
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
